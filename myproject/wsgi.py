7412684bd751cd54733d3d21257380cc390236c3
unknown <sergiojm2003@gmail.com>
2024-12-04 20:00:03 +0100

    god

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -16,7 +16,7 @@ if path not in sys.path:
     sys.path.append(path)
 
 # Establecer el DJANGO_SETTINGS_MODULE
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Improvent.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
 # Obtener la aplicación WSGI de Django
 from django.core.wsgi import get_wsgi_application
